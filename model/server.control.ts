56c38a4e73e37177fa565ca1e595156386264ea7
Jossue <jativatamayojossuedavid@gmail.com>
2025-05-05 10:51:47 -0500

    VOlver al ||

--- model/server.control.ts
+++ model/server.control.ts
@@ -14,7 +14,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT ?? '3000'; // Use environment variable for port
+        this.port = process.env.PORT || '3000'; // Use environment variable for port
         this.server = http.createServer(this.app);
         this.io = new ServerSocket(this.server, {
             cors: {
